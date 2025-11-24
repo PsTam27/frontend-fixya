9c42b25ce8c721269a75e74b5cc7e3fd1df6d098
PsTam27 <pi.sat@duocuc.cl>
2025-11-24 12:07:59 -0300

    Modificando css de registro user

--- app/(unregister)/(user-register)/step1.tsx
+++ app/(unregister)/(user-register)/step1.tsx
@@ -1,3 +1,5 @@
+
+
 import { errorColor } from "@/constants/theme"
 import { registerUserSchema } from "@/models/user/payload"
 import { UserTypeEnum } from "@/models/user/response"
@@ -39,12 +41,12 @@ import { Dropdown } from "react-native-element-dropdown"
 
 export default function UserRegisterStep1() {
   const {
-          control,
-          handleSubmit,
-          getValues,
-          formState: { errors },
-          watch
-        } = useForm( {
+           control,
+           handleSubmit,
+           getValues,
+           formState: { errors },
+           watch
+         } = useForm( {
     resolver     : zodResolver( registerUserSchema.extend( {
       password: passwordSchema,
       confirm : z.string( {
@@ -86,13 +88,13 @@ export default function UserRegisterStep1() {
     enabled : Boolean( regionSelected )
   } )
 
-  const parsedComunas                   = comunaData?.map( ( comuna ) => (
+  const parsedComunas              = comunaData?.map( ( comuna ) => (
     {
       label: comuna.name,
       value: String( comuna.id )
     }
   ) ) || []
-  const { data }                        = useQuery( regionsOption )
+  const { data }                   = useQuery( regionsOption )
   const [showPassword, setShowPassword] = useState( false )
 
   const parsedRegions = data?.map( ( region ) => (
@@ -102,10 +104,10 @@ export default function UserRegisterStep1() {
     }
   ) ) || []
 
-  const { register }                        = useAuth()
-  const router                              = useRouter()
-  const [submiting, startSubmitting]        = useState( false )
-  const onSubmit                            = async ( data: any ) => {
+  const { register }             = useAuth()
+  const router                   = useRouter()
+  const [submiting, startSubmitting]   = useState( false )
+  const onSubmit                   = async ( data: any ) => {
     startSubmitting( true )
     const result = await register( data, data.password )
     startSubmitting( false )
@@ -136,14 +138,17 @@ export default function UserRegisterStep1() {
             control={ control }
             name="full_name"
             render={ ( { field: { onChange, onBlur, value } } ) => (
-              <TextInput
-                editable={ !submiting }
-                style={ styles.input } placeholder="Nombre completo"
-                onBlur={ onBlur }
-                onChangeText={ onChange }
-                value={ value }
-                placeholderTextColor="#999"
-              />
+              <>
+                <Text style={styles.inputLabel}>Nombre completo</Text>
+                <TextInput
+                  editable={ !submiting }
+                  style={ styles.input } placeholder="" // Placeholder vacío
+                  onBlur={ onBlur }
+                  onChangeText={ onChange }
+                  value={ value }
+                  placeholderTextColor="#999"
+                />
+              </>
             ) }
           />
           { errors.full_name && <Text
@@ -152,15 +157,18 @@ export default function UserRegisterStep1() {
             control={ control }
             name="email"
             render={ ( { field: { onChange, onBlur, value } } ) => (
-              <TextInput
-                editable={ !submiting }
-                style={ styles.input } placeholder="Correo electronico"
-                onBlur={ onBlur }
-                onChangeText={ onChange }
-                value={ value }
-                keyboardType="email-address" autoCapitalize="none"
-                placeholderTextColor="#999"
-              />
+              <>
+                <Text style={styles.inputLabel}>Correo electrónico</Text>
+                <TextInput
+                  editable={ !submiting }
+                  style={ styles.input } placeholder="" // Placeholder vacío
+                  onBlur={ onBlur }
+                  onChangeText={ onChange }
+                  value={ value }
+                  keyboardType="email-address" autoCapitalize="none"
+                  placeholderTextColor="#999"
+                />
+              </>
             ) }
           />
           { errors.email && <Text
@@ -169,12 +177,15 @@ export default function UserRegisterStep1() {
             control={ control }
             name="bank_identity"
             render={ ( { field: { onChange, onBlur, value } } ) => (
-              <TextInput style={ styles.input } placeholder="Rut"
-                         editable={ !submiting }
-                         onBlur={ onBlur }
-                         onChangeText={ onChange }
-                         value={ value }
-                         placeholderTextColor="#999"/>
+              <>
+                <Text style={styles.inputLabel}>Rut</Text>
+                <TextInput style={ styles.input } placeholder=""
+                  editable={ !submiting }
+                  onBlur={ onBlur }
+                  onChangeText={ onChange }
+                  value={ value }
+                  placeholderTextColor="#999"/>
+              </>
             ) }
           />
           { errors.bank_identity && <Text
@@ -183,13 +194,16 @@ export default function UserRegisterStep1() {
             control={ control }
             name="phone"
             render={ ( { field: { onChange, onBlur, value } } ) => (
-              <TextInput style={ styles.input } placeholder="Celular"
-                         editable={ !submiting }
-                         onBlur={ onBlur }
-                         onChangeText={ onChange }
-                         value={ value }
-                         keyboardType="phone-pad"
-                         placeholderTextColor="#999"/>
+              <>
+                <Text style={styles.inputLabel}>Celular</Text>
+                <TextInput style={ styles.input } placeholder=""
+                  editable={ !submiting }
+                  onBlur={ onBlur }
+                  onChangeText={ onChange }
+                  value={ value }
+                  keyboardType="phone-pad"
+                  placeholderTextColor="#999"/>
+              </>
             ) }
           />
           { errors.phone && <Text
@@ -201,13 +215,14 @@ export default function UserRegisterStep1() {
               const dateValue = value ? new Date( value ) : undefined
               return (
                 <>
+                  <Text style={styles.inputLabel}>Fecha de nacimiento</Text>
                   <Pressable disabled={ submiting }
-                             onPress={ () => setShowDatePicker( true ) }
-                             style={ styles.input }>
+                    onPress={ () => setShowDatePicker( true ) }
+                    style={ styles.inputDate }>
                     <Text style={ { color: value ? "#000" : "#999" } }>
                       { value
                         ? dateValue!.toLocaleDateString()
-                        : "Fecha de nacimiento" }
+                        : "Selecciona fecha" }
                     </Text>
                   </Pressable>
 
@@ -242,28 +257,31 @@ export default function UserRegisterStep1() {
             control={ control }
             name="region"
             render={ ( { field: { onChange, onBlur, value } } ) => (
-              <Dropdown
-                disable={ submiting }
-                style={ [styles.dropdown] }
-                placeholderStyle={ styles.placeholderStyle }
-                selectedTextStyle={ styles.selectedTextStyle }
-                inputSearchStyle={ styles.inputSearchStyle }
-                iconStyle={ styles.iconStyle }
-                itemTextStyle={ styles.itemTextStyle }
-                data={ parsedRegions }
-                search
-                maxHeight={ 300 }
-                labelField="label"
-                valueField="value"
-                placeholder="Selecciona una region"
-                searchPlaceholder="Buscar..."
-                value={ value }
-                onBlur={ onBlur }
-                onChange={ item => {
-                  // const selected = data?.find( region => region.id === item.value )
-                  onChange( item.value )
-                } }
-              />
+              <>
+                <Text style={styles.inputLabel}>Región</Text>
+                <Dropdown
+                  disable={ submiting }
+                  style={ [styles.dropdown] }
+                  placeholderStyle={ styles.placeholderStyle }
+                  selectedTextStyle={ styles.selectedTextStyle }
+                  inputSearchStyle={ styles.inputSearchStyle }
+                  iconStyle={ styles.iconStyle }
+                  itemTextStyle={ styles.itemTextStyle }
+                  data={ parsedRegions }
+                  search
+                  maxHeight={ 300 }
+                  labelField="label"
+                  valueField="value"
+                  placeholder="Selecciona una region"
+                  searchPlaceholder="Buscar..."
+                  value={ value }
+                  onBlur={ onBlur }
+                  onChange={ item => {
+                    // const selected = data?.find( region => region.id === item.value )
+                    onChange( item.value )
+                  } }
+                />
+              </>
             ) }
           />
           { errors.region && <Text
@@ -272,30 +290,33 @@ export default function UserRegisterStep1() {
             control={ control }
             name="comuna"
             render={ ( { field: { onChange, onBlur, value } } ) => (
-              <Dropdown
-                style={ [styles.dropdown] }
-                disable={ !regionSelected || submiting }
-                placeholderStyle={ styles.placeholderStyle }
-                selectedTextStyle={ styles.selectedTextStyle }
-                inputSearchStyle={ styles.inputSearchStyle }
-                iconStyle={ styles.iconStyle }
-                itemTextStyle={ styles.itemTextStyle }
-                data={ parsedComunas }
-                search
-                maxHeight={ 300 }
-                labelField="label"
-                valueField="value"
-                placeholder={ regionSelected
-                  ? "Selecciona una comuna"
-                  : "Selecciona una región primero" }
-                searchPlaceholder="Buscar..."
-                value={ value }
-                onBlur={ onBlur }
-                onChange={ item => {
-                  // const selected = comunaData?.find( comuna => comuna.id === item.value )
-                  onChange( item.value )
-                } }
-              />
+              <>
+                <Text style={styles.inputLabel}>Comuna</Text>
+                <Dropdown
+                  style={ [styles.dropdown] }
+                  disable={ !regionSelected || submiting }
+                  placeholderStyle={ styles.placeholderStyle }
+                  selectedTextStyle={ styles.selectedTextStyle }
+                  inputSearchStyle={ styles.inputSearchStyle }
+                  iconStyle={ styles.iconStyle }
+                  itemTextStyle={ styles.itemTextStyle }
+                  data={ parsedComunas }
+                  search
+                  maxHeight={ 300 }
+                  labelField="label"
+                  valueField="value"
+                  placeholder={ regionSelected
+                    ? "Selecciona una comuna"
+                    : "Selecciona una región primero" }
+                  searchPlaceholder="Buscar..."
+                  value={ value }
+                  onBlur={ onBlur }
+                  onChange={ item => {
+                    // const selected = comunaData?.find( comuna => comuna.id === item.value )
+                    onChange( item.value )
+                  } }
+                />
+              </>
             ) }
           />
           { errors.comuna && <Text
@@ -304,12 +325,15 @@ export default function UserRegisterStep1() {
             control={ control }
             name="calle"
             render={ ( { field: { onChange, onBlur, value } } ) => (
-              <TextInput style={ styles.input } placeholder="Calle"
-                         editable={ !submiting }
-                         onBlur={ onBlur }
-                         onChangeText={ onChange }
-                         value={ value }
-                         placeholderTextColor="#999"/>
+              <>
+                <Text style={styles.inputLabel}>Dirección</Text>
+                <TextInput style={ styles.input } placeholder=""
+                  editable={ !submiting }
+                  onBlur={ onBlur }
+                  onChangeText={ onChange }
+                  value={ value }
+                  placeholderTextColor="#999"/>
+              </>
             ) }
           />
           { errors.calle && <Text
@@ -323,10 +347,10 @@ export default function UserRegisterStep1() {
                 alignItems   : "center",
                 marginBottom : 20
               } }>
+                <Text style={styles.inputLabel}>Contraseña</Text>
                 <TextInput
                   style={ [styles.input, { flex: 1, marginBottom: 0 }] }
                   editable={ !submiting }
-                  placeholder="Contraseña"
                   secureTextEntry={ !showPassword }
                   onBlur={ onBlur }
                   onChangeText={ onChange }
@@ -362,10 +386,10 @@ export default function UserRegisterStep1() {
                 alignItems   : "center",
                 marginBottom : 20
               } }>
+                <Text style={styles.inputLabel}>Repetir Contraseña</Text>
                 <TextInput
                   style={ [styles.input, { flex: 1, marginBottom: 0 }] }
                   editable={ !submiting }
-                  placeholder="Confirmar Contraseña"
                   secureTextEntry={ !showPassword }
                   onBlur={ onBlur }
                   onChangeText={ onChange }
@@ -398,7 +422,7 @@ export default function UserRegisterStep1() {
           <Pressable
             disabled={submiting}
             style={ styles.button }
-                     onPress={ handleSubmit( onSubmit ) }>
+                      onPress={ handleSubmit( onSubmit ) }>
             <Text style={ styles.buttonText }>
               { submiting ? "Creando cuenta..." : "Crear" }
             </Text>
@@ -413,7 +437,7 @@ const styles = StyleSheet.create( {
   container        : {
     flex           : 1,
     backgroundColor: "#F7F8FA",
-    padding        : 16
+    padding        : 0
   },
   dropdown         : {
     height           : 50,
@@ -443,7 +467,7 @@ const styles = StyleSheet.create( {
     textTransform: "capitalize"
   },
   selectedTextStyle: {
-    fontSize     : 16,
+    fontSize       : 16,
     textTransform: "capitalize"
 
   },
@@ -463,19 +487,35 @@ const styles = StyleSheet.create( {
     paddingBottom    : 40
   },
   headerTitle      : {
-    fontSize    : 24,
-    fontWeight  : "bold",
-    color       : "#2C3E50",
-    textAlign   : "center",
-    marginTop   : 10,
+    fontSize  : 24,
+    fontWeight: "bold",
+    color     : "#2C3E50",
+    textAlign : "center",
+    marginTop : 10,
     marginBottom: 15
+  },
+  inputLabel       : { 
+    fontSize: 14,
+    color: "#555",
+    marginBottom: 5,
+    fontWeight: "500",
   },
   input            : {
     fontSize         : 16,
     paddingVertical  : 15,
     borderBottomWidth: 1,
     borderBottomColor: "#DDD",
-    marginBottom     : 20
+    marginBottom     : 20,
+    paddingHorizontal: 0, 
+  },
+  inputDate        : { 
+    fontSize         : 16,
+    paddingVertical  : 15,
+    borderBottomWidth: 1,
+    borderBottomColor: "#DDD",
+    marginBottom     : 20,
+    justifyContent   : "center",
+    height           : 50
   },
   button           : {
     backgroundColor: "#3498DB",
